29c1a5a57330f4db18b189f83c2f139af6e02c86
Maximilian Stock <mxmlnstock@googlemail.com>
2022-11-02 21:35:59 +0100

    chore: add ci benchmark step

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -2,17 +2,41 @@
 
 mkdir -p benchmarks/{basic-local,threaded-4-local,tokio-local}
 
+DOMAINS="https://raw.githubusercontent.com/Tantalor93/dnspyre/master/data/2-domains"
+
+cargo build --release
+
 # dns-block
-dnspyre -s "127.0.0.1:53000" -n 1 -t A --distribution --csv benchmarks/basic-local-release.csv \
+cargo run --release -p dns-block &
+sleep 2
+dnspyre -s "127.0.0.1:53000" -n 1 -t A \
+    --distribution \
+    --csv benchmarks/basic-local-release.csv \
     --plot benchmarks/basic-local \
-    --codes "https://raw.githubusercontent.com/Tantalor93/dnspyre/master/data/1000-domains" > benchmarks/basic-local/basic-local-release
+    --codes $DOMAINS \
+    > benchmarks/basic-local/basic-local-release
+pkill dns-block
+sleep 2
 
 # dns-block-threaded
-dnspyre -s "127.0.0.1:53001" -n 1 -t A --distribution --csv benchmarks/threaded-4-local-release.csv \
+PORT=53001 cargo run --release -p dns-block-threaded &
+sleep 2
+dnspyre -s "127.0.0.1:53001" -n 1 -t A \
+    --distribution \
+    --csv benchmarks/threaded-4-local-release.csv \
     --plot benchmarks/threaded-4-local \
-    --codes "https://raw.githubusercontent.com/Tantalor93/dnspyre/master/data/1000-domains" > benchmarks/threaded-4-local/threaded-4-local-release
+    --codes $DOMAINS \
+    > benchmarks/threaded-4-local/threaded-4-local-release
+pkill dns-block-threaded
+sleep 2
 
 # dns-block-tokio
-dnspyre -s "127.0.0.1:53002" -n 1 -t A --distribution --csv benchmarks/tokio-local-release.csv \
+PORT=53002 cargo run --release -p dns-block-tokio &
+sleep 2
+dnspyre -s "127.0.0.1:53002" -n 1 -t A \
+    --distribution \
+    --csv benchmarks/tokio-local-release.csv \
     --plot benchmarks/tokio-local \
-    --codes "https://raw.githubusercontent.com/Tantalor93/dnspyre/master/data/1000-domains" > benchmarks/tokio-local/tokio-local-release
+    --codes $DOMAINS \
+    > benchmarks/tokio-local/tokio-local-release
+pkill dns-block-tokio
