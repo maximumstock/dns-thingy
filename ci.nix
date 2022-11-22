47854e4562dd26acb0cc63673efb0b36d6d4e2f5
Maximilian Stock <mxmlnstock@googlemail.com>
2022-11-22 17:59:03 +0100

    chore: refactor ci benchmark step

---
+++ ci.nix
@@ -0,0 +1,17 @@
+let
+  rustOverlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  pkgs = import <nixpkgs> {
+    overlays = [ (import rustOverlay) ];
+  };
+  dnspyre = import ./dnspyre.nix { };
+in
+pkgs.mkShell rec {
+  buildInputs = with pkgs; [
+    (rust-bin.stable."1.65.0".default.override {
+      extensions = [ "rust-src" "clippy" ];
+    })
+    dnspyre
+  ];
+
+  RUST_BACKTRACE = 1;
+}
