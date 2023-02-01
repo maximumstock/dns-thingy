adb6ae1efdf7ad2ef2e717f73b4180bf21d6f2b5
Maximilian Stock <maximilian@wonder.me>
2023-02-01 18:58:53 +0100

    refactor(dns-block-threaded)

--- crates/dns-block-threaded/src/main.rs
+++ crates/dns-block-threaded/src/main.rs
@@ -18,29 +18,24 @@ fn main() {
     let internal_socket = UdpSocket::bind(("0.0.0.0", port)).unwrap();
     let external_socket = UdpSocket::bind(("0.0.0.0", 0)).unwrap();
 
-    println!("Started DNS blocker on 127.0.0.1::{}", port);
+    println!("Started DNS blocker on 127.0.0.1::{port}");
 
     let mut handles = vec![];
 
     for _ in 0..4 {
-        let socket = internal_socket.try_clone().unwrap();
+        let internal_socket = internal_socket.try_clone().unwrap();
         let external_socket = external_socket.try_clone().unwrap();
         let dns = Arc::clone(&dns);
+        let mut incoming_query = [0; 512];
+
         let handle = std::thread::spawn(move || loop {
-            let mut incoming_query = [0; 512];
-            let (_, sender) = socket.recv_from(&mut incoming_query).unwrap();
-            match resolve_pipe(
-                &incoming_query,
-                (dns.as_str(), 53),
-                Some(external_socket.try_clone().unwrap()),
-            ) {
-                Ok((_, reply)) => {
-                    socket.send_to(&reply, sender).unwrap();
-                }
-                Err(e) => {
-                    dbg!(e);
-                }
-            }
+            process(
+                &internal_socket,
+                &mut incoming_query,
+                &dns,
+                &external_socket,
+            );
+            incoming_query.fill(0);
         });
 
         handles.push(handle);
@@ -48,5 +43,34 @@ fn main() {
 
     for handle in handles {
         handle.join().unwrap();
+    }
+}
+
+fn process(
+    internal_socket: &UdpSocket,
+    incoming_query: &mut [u8; 512],
+    dns: &Arc<String>,
+    external_socket: &UdpSocket,
+) {
+    let (_, sender) = internal_socket.recv_from(incoming_query).unwrap();
+    let (request_id, question) = parse_query(*incoming_query).unwrap();
+
+    if apply_domain_filter(&question.domain_name) {
+        println!("Blocking request for {:?}", question.domain_name);
+        let nx_response = generate_response(request_id, dns::dns::ResponseCode::NXDOMAIN).unwrap();
+        internal_socket.send_to(&nx_response, sender).unwrap();
+    } else {
+        match resolve_pipe(
+            &*incoming_query,
+            (dns.as_str(), 53),
+            Some(external_socket.try_clone().unwrap()),
+        ) {
+            Ok((_, reply)) => {
+                internal_socket.send_to(&reply, sender).unwrap();
+            }
+            Err(e) => {
+                dbg!(e);
+            }
+        }
     }
 }
