896b4b9c8c29b3d69bdcd64014dd3572195e1982
Maximilian Stock <mxmlnstock@googlemail.com>
2023-04-01 02:23:55 +0200

    chore: fix annotate-pr-benchmark-link.sh

--- scripts/annotate-pr-benchmark-link.sh
+++ scripts/annotate-pr-benchmark-link.sh
@@ -25,7 +25,53 @@ echo "Branch Benchmark: $BRANCH_BENCHMARKS_URL"
 
 PR_NUMBER=$(gh api "/repos/maximumstock/dns-thingy/pulls?head=$BRANCH&per_page=1" | jq ".[0].number")
 
+gh run download $MASTER_RUN_ID -n benchmark-results --dir master-benchmark-results
+gh run download $BRANCH_RUN_ID -n benchmark-results --dir branch-benchmark-results
+
+MASTER_BASIC_STDOUT=$(cat master-benchmark-results/basic/stdout)
+MASTER_THREADED_STDOUT=$(cat master-benchmark-results/threaded-4/stdout)
+MASTER_TOKIO_STDOUT=$(cat master-benchmark-results/tokio/stdout)
+BRANCH_BASIC_STDOUT=$(cat branch-benchmark-results/basic/stdout)
+BRANCH_THREADED_STDOUT=$(cat branch-benchmark-results/threaded-4/stdout)
+BRANCH_TOKIO_STDOUT=$(cat branch-benchmark-results/tokio/stdout)
+
 gh pr comment $PR_NUMBER --body "
   - Master Benchmark: $MASTER_BENCHMARKS_URL
   - Branch Benchmark: $BRANCH_BENCHMARKS_URL
+
+  # Basic Old
+
+  \`\`\`
+  $MASTER_BASIC_STDOUT
+  \`\`\`
+
+  # Basic New
+
+  \`\`\`
+  $BRANCH_BASIC_STDOUT
+  \`\`\`
+
+  ## Threaded Old
+
+  \`\`\`
+  $MASTER_THREADED_STDOUT
+  \`\`\`
+
+  ## Threaded New
+
+  \`\`\`
+  $BRANCH_THREADED_STDOUT
+  \`\`\`
+
+  ## Tokio Old
+
+  \`\`\`
+  $MASTER_TOKIO_STDOUT
+  \`\`\`
+
+  ## Tokio New
+
+  \`\`\`
+  $BRANCH_TOKIO_STDOUT
+  \`\`\`
   "
