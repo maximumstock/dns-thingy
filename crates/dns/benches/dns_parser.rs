ad5885f4647f50d7de1ab4f71730c7ead71eb589
Maximilian Stock <mxmlnstock@googlemail.com>
2023-04-04 22:27:37 +0200

    chore: add dns parser criterion benchmark

--- crates/dns/benches/dns_parser.rs
+++ crates/dns/benches/dns_parser.rs
@@ -1,15 +1,30 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use dns::dns::DnsParser;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn dns_parser(c: &mut Criterion) {
+    let dns_queries: Vec<_> = (include_bytes!("./inputs/1000.bin").chunks(512))
+        .chain(include_bytes!("./inputs/youtube-spotify.bin").chunks(512))
+        .map(|c| c.to_vec())
+        .collect();
+
     c.bench_function("question parsing", |b| {
-        b.iter(|| DnsParser::new(black_box(vec![])).parse_question())
+        let queries = dns_queries.clone();
+        b.iter(|| {
+            for p in queries.clone().into_iter() {
+                DnsParser::new(black_box(p)).parse_question();
+            }
+        });
     });
 
     c.bench_function("answers parsing", |b| {
-        b.iter(|| DnsParser::new(black_box(vec![])).parse_answers())
+        let queries = dns_queries.clone();
+        b.iter(|| {
+            for p in queries.clone().into_iter() {
+                DnsParser::new(black_box(p)).parse_answers().unwrap();
+            }
+        });
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, dns_parser);
 criterion_main!(benches);
