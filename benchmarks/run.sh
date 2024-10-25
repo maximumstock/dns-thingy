78b6db2bc6d5dea80f4d9bf1e195ee690c02ae12
Maximilian Stock <mxmlnstock@gmail.com>
2024-10-25 15:37:43 +0200

    chore(ci): enable benchmarks

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -7,20 +7,20 @@ DOMAINS_100=$(curl -s "$DOMAINS_URL" | head -n 100)
 
 export PATH="./releases:./target/release:$PATH"
 
-mkdir -p benchmarks/tokio
+mkdir -p benchmarks
 
-dns-block-tokio --benchmark --port 53002 --quiet &
+dns-block-tokio --benchmark --resolution-delay-ms 10 --bind-port 53000 --quiet &
 echo "Started dns-block-tokio"
 
 sleep 3
 
 # dns-block-tokio
 echo "Starting dns-block-tokio benchmark"
-echo $DOMAINS_100 | xargs dnspyre -s "127.0.0.1:53002" -n 1 -c 3 -t A \
+echo $DOMAINS_100 | xargs dnspyre -s "127.0.0.1:53000" -n 1 -c 3 -t A \
     --recurse \
-    --csv benchmarks/tokio/raw.csv \
-    --plot benchmarks/tokio \
-    > benchmarks/tokio/stdout
+    --csv benchmarksraw.csv \
+    --plot benchmarks \
+    > benchmarks/stdout
 
 echo "Killing servers..."
 pkill dns-block-tokio || true
