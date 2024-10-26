e4f6c565e69f1d719806f9d655036027de72c5c6
Maximilian Stock <mxmlnstock@gmail.com>
2024-10-26 14:11:11 +0200

    chore(ci): perf collect stack traces

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -19,9 +19,7 @@ echo "Starting perf recording"
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sudo sysctl -w kernel.perf_event_paranoid=1
 fi
-perf record -F99 --call-graph dwarf -p $(pgrep dns-block-tokio) -o $OUTPUT_PATH/perf.data &
-ls -lah
-tree $OUTPUT_PATH
+perf record -F99 -a --call-graph dwarf -p $(pgrep dns-block-tokio) -o $OUTPUT_PATH/perf.data &
 
 echo "Starting dns-block-tokio benchmark"
 echo $DOMAINS_100 | xargs dnspyre -s "127.0.0.1:53000" -n 1 -c 3 -t A \
@@ -34,3 +32,9 @@ echo $DOMAINS_100 | xargs dnspyre -s "127.0.0.1:53000" -n 1 -c 3 -t A \
 echo "Killing servers..."
 pkill -SIGTERM perf || true
 pkill -SIGTERM dns-block-tokio || true
+
+git clone --depth 1 http://github.com/brendangregg/FlameGraph
+cd FlameGraph
+./stackcollapse-perf.pl ../out.stacks1 > out.folded1
+./stackcollapse-perf.pl ../out.stacks2 > out.folded2
+./difffolded.pl out.folded1 out.folded2 | ./flamegraph.pl > diff2.svg
