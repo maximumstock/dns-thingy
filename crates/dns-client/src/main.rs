ca773f7a6000ca0295493ecc37196d01473bac4c
Maximilian Stock <mxmlnstock@gmail.com>
2024-11-03 17:36:42 +0100

    fix(dns-client): support new DNS record types

--- crates/dns-client/src/main.rs
+++ crates/dns-client/src/main.rs
@@ -15,6 +15,25 @@ fn main() {
         match answer {
             Answer::A { meta, ipv4 } => println!("A\t{meta:?} - {ipv4}"),
             Answer::CNAME { meta, cname } => println!("CNAME\t{meta:?} - {cname}"),
+            Answer::AAAA { meta, ipv6 } => println!("CNAME\t{meta:?} - {ipv6}"),
+            Answer::NS { ns, meta } => println!("CNAME\t{meta:?} - {ns}"),
+            Answer::MB { domain_name, meta } => println!("CNAME\t{meta:?} - {domain_name}"),
+            Answer::MX {
+                preference,
+                exchange,
+                meta,
+            } => println!("CNAME\t{meta:?} - {exchange} ({preference})"),
+            Answer::PTR { domain_name, meta } => println!("CNAME\t{meta:?} - {domain_name}"),
+            Answer::SOA {
+                meta,
+                mname,
+                rname,
+                serial: _,
+                refresh: _,
+                retry: _,
+                expire: _,
+                minimum: _,
+            } => println!("CNAME\t{meta:?} - {mname} - {rname}"),
         }
     }
 }
